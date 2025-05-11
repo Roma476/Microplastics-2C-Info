28a731afcba395ef0d59dda36176d587bdae4958
Roma476 <friciu.robert09@gmail.com>
2025-05-11 21:17:51 +0200

    Modifiche HOVER
    -Modificati i hover delle cards

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 window.addEventListener("load", function () {
   const preloader = document.getElementById("preloader");
 
-  // Imposta il minimo tempo di visualizzazione (2000ms)
-  const minimumTime = 5000; // Tempo massimo di visualizzazione per il loader (5 secondi)
+  const durationDesktop = 3000; 
+  const durationMobile = 5000; 
+
   const startTime = performance.timing.navigationStart;
   const currentTime = Date.now();
   const elapsed = currentTime - startTime;
-  const remainingTime = Math.max(minimumTime - elapsed, 0);
 
-  // Aggiungi un ritardo di almeno 2 secondi prima di rimuovere il loader
+  let remainingTime;
+  if (window.innerWidth <= 768) { // Mobile
+    remainingTime = Math.max(durationMobile - elapsed, 0);
+  } else { // Desktop
+    remainingTime = Math.max(durationDesktop - elapsed, 0);
+  }
+
   setTimeout(() => {
-    preloader.style.transition = "opacity 0.5s ease-out";  // Aggiungi una transizione
-    preloader.style.opacity = "0";
-    
-    // Dopo che l'animazione è finita, rimuoviamo il loader
+    preloader.style.opacity = 0; 
     setTimeout(() => {
-      preloader.remove();
-    }, 500); // Tempo che dura la transizione (500ms)
-  }, remainingTime);
+      preloader.style.display = "none"; ì
+    }, 500); ì
+  }, remainingTime); 
 });
