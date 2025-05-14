56d76115f3f0cd6391dcf7f3dbc76e9de94d5eb3
Roma476 <friciu.robert09@gmail.com>
2025-05-14 21:20:40 +0200

    Aggiunta Pagina gruppo Matteo

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
 window.addEventListener("load", function () {
   const preloader = document.getElementById("preloader");
 
-  const durationDesktop = 3000; 
-  const durationMobile = 5000; 
+  const durationDesktop = 3000;
+  const durationMobile = 5000;
 
   const startTime = performance.timing.navigationStart;
   const currentTime = Date.now();
   const elapsed = currentTime - startTime;
 
   let remainingTime;
-  if (window.innerWidth <= 768) { // Mobile
+  if (window.innerWidth <= 768) {
     remainingTime = Math.max(durationMobile - elapsed, 0);
-  } else { // Desktop
+  } else {
     remainingTime = Math.max(durationDesktop - elapsed, 0);
   }
 
   setTimeout(() => {
-    preloader.style.opacity = 0; 
+    preloader.style.opacity = 0;
     setTimeout(() => {
-      preloader.style.display = "none"; ì
-    }, 500); ì
-  }, remainingTime); 
+      preloader.style.display = "none";
+    }, 500);
+  }, remainingTime);
+
+  // Navbar scroll 
+  let lastScroll = 0;
+  const nav = document.querySelector("nav");
+
+  if (window.innerWidth > 768) {
+    window.addEventListener("scroll", () => {
+      const currentScroll = window.pageYOffset;
+
+      const scrollThreshold = window.innerHeight * 0.9; 
+
+      if (currentScroll > lastScroll && currentScroll > scrollThreshold) {
+        nav.style.transform = "translateY(-200%)";
+      } else if (currentScroll < lastScroll && currentScroll > scrollThreshold) {
+        nav.style.transform = "translateY(0)";
+      }
+
+      lastScroll = currentScroll;
+    });
+  } else {
+    nav.style.transform = "translateX(-50%)";
+  }
 });
